285d6e9544c8c3329dfac610b1a698fc1ed0fc33
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-09-12 17:03:42 +0900

    edit for index & show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,9 +17,6 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-  end
-
   private
   def post_params
     params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
