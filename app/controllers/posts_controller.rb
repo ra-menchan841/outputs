f81c9e6bbff9050b83554e6db5762656b3fb860d
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-09-29 17:42:17 +0900

    add random function in posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    comment = Comment.pluck(:name).sample(6).first
     if @post.save
-      redirect_to posts_path, notice: 'アウトプットしたの？えらい！'
+      redirect_to posts_path, notice: " #{comment} "
     else
-      redirect_to posts_path, notice: '失敗しちゃった、、、＞＜'
+      redirect_to posts_path, notice: " 失敗しちゃった、、、＞＜ "
     end
   end
 
