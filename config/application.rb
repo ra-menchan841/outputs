89b143e4175af572820df714a7cb55a42a849fbc
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-12 23:08:54 +0900

    add conform popup for deleting post, and edit valideation for post title

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Output
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
