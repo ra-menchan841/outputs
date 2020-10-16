80f54a9aabfe7cc276a5fd055c0be2cc9a2d6972
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-16 17:23:00 +0900

    edit css in new & edit views

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :user do
     nickname              {"テスト"}
     email                 {"test@gmail.com"}
-    password              {"00000000"}
+    password              {"000000"}
     password_confirmation {"00000000"}
   end
 
